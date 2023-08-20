70511fe315bb0dcc2bb0db6c115a32319ec873a4
dhanabharathi <dhanabharathi77@gmail.com>
2023-08-20 22:02:30 +0530

    Recipe image button added and the recipe Xml file is added

--- app/src/main/java/com/bharathi/dinnersdecider/SplashActivity.kt
+++ app/src/main/java/com/bharathi/dinnersdecider/SplashActivity.kt
@@ -8,24 +8,15 @@ import android.content.Intent
 
 class SplashActivity : AppCompatActivity() {
 
-    val splashTimeout:Long = 2000
+    val splashTimeout: Long = 2000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splashscreen)
-
-        /* Handler().postDelayed({
-
-            val nextscreen = Intent(this, MainActivity::class.java)
-            startActivity(nextscreen)
-            finish()
-
-        }, 2000) */
-
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             finish()
-        } , splashTimeout )
+        }, splashTimeout)
     }
 
 }
